c63d5ecd0fe9dcc8e1842075020c3e951534e94a
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-06-05 19:43:29 +0900

    mvc(2)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("new MVC");
+        System.out.println("new MVC2");
         System.out.println("Hello world!");
     }
 }
